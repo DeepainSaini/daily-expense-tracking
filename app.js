ebe0fe8788ed9abbe2c58efdc23ef1cf30aaf866
DeepainSaini <deepainsaini111@gmail.com>
2026-01-17 12:45:29 +0530

    removing logs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express  = require('express');
 const db = require('./util/db-connection.js');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 const app  = express();
 
 const userRoute = require('./routes/userRoutes.js');
@@ -8,20 +10,21 @@ const paymentRoute = require('./routes/paymentRoutes.js');
 const premiumRoute = require('./routes/premiumRoutes.js');
 const passwordRoute = require('./routes/passwordRoutes.js');
 require('./models');
-// const userModel = require('./models/users.js');
-// const expenseModel = require('./models/expenses.js');
+
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(cookieParser());
+
 
 app.use('/',userRoute);
 app.use('/',expenseRoute);
 app.use('/',paymentRoute);
 app.use('/premium',premiumRoute);
 app.use('/called',passwordRoute);
+app.use(express.static('public'));
 
 db.sync({force:false}).then(()=>{
     app.listen(3000,(err)=>{
